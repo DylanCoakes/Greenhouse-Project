4c54841a9c71df205577435d08d057e7ca6b0758
DylanCoakes <dylan.coakes@lethbridgecollege.ca>
2021-12-13 19:15:01 -0700

    Recovered project. Added some audio sources and materials. added object board after it was delted.
    Not everything was brought over from recovered project.

---
+++ Assets/Scripts/AudioTrigger.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioTrigger : MonoBehaviour
+{
+    public AudioSource triggerAudio;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        triggerAudio.Play();
+    }
+
+
+
+
+}
